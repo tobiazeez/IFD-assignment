651ae7ac9dcb25689329188820e9ee41bec0b1d2
tobiazeez <93913346+tobiazeez@users.noreply.github.com>
2022-06-30 15:56:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import { EndScreen } from './components/EndScreen';
 
 export const App = () => {
     const [gameState, setGameState] = useState(0);
-    const [round, setRound] = useState('3'); 
+    const [round, setRound] = useState('3');
+    const [time, setTime] = useState(Date.now());
 
 
     return (
         <div>
-            {gameState === 0 && <StartScreen setGameState = {setGameState} round = {round} setRound = {setRound}/>}
+            {gameState === 0 && <StartScreen setGameState = {setGameState} round = {round} setRound = {setRound} setTime = {setTime}/>}
 
             {gameState === 1 && <PlayScreen setGameState = {setGameState} round = {round} setRound = {setRound}/>}
         
-            {gameState === 2 && <EndScreen setGameState = {setGameState}/>}
+            {gameState === 2 && <EndScreen setGameState = {setGameState} time = {time}/>}
         </div>
     );
     
